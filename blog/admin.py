70ffd4d2792567cdb9ed47d2421f2544f1038c5a
Aliou Bop <bopaliou1994@gmail.com>
2022-02-24 17:20:52 +0100

    add models and configure admin page

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from blog.models import Post,Category
 
-# Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at', 'last_modified')
+    list_filter = ('created_at',)
+    search_fields = ('title', 'body')
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
