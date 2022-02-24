39a46c81042be7074e6bc464610d752877126d01
Aliou Bop <bopaliou1994@gmail.com>
2022-02-24 13:39:42 +0100

    Add Views:project_index and project_detail

--- myprojects/views.py
+++ myprojects/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from myprojects.models import Project
+def project_index(request):
+    projects=Project.objects.all()
+    context={
+        'projects':projects
+    }
+    return render(request, "myprojects/index.html",context)
 
-# Create your views here.
+def project_detail(request,pk):
+    project=Project.objects.get(pk=pk)
+    context={
+        'project':project
+    }
+    return render(request, "myprojects/detail.html",context)
