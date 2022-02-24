c387a11f2be92c1ba5af94999386bcad7a7a2be0
Aliou Bop <bopaliou1994@gmail.com>
2022-02-24 18:00:44 +0100

    add views and configure routes

---
+++ blog/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+
+class CommentForm(forms.Form):
+    author=forms.CharField(max_length=80,widget=forms.TextInput(
+                                attrs={'class':'form-control','placeholder':'Your name'})),
+    body=forms.CharField(widget=forms.Textarea(
+        attrs={'class':'form-control','placeholder':'Leave a comment!'}))
+    )
